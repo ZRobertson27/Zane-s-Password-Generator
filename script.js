1425dc277dfbb29169087bfdee987a67f52984e7
ZRobertson27 <115191138+ZRobertson27@users.noreply.github.com>
2022-11-10 13:31:09 -0600

    Added prompts for password criteria.

--- script.js
+++ script.js
@@ -3,15 +3,36 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("hello")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-  window.prompt(Test);
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  var passLength = window.prompt("How long would you like your password? Min: 8 characters Max: 128 characters.");
+ 
+  if (!passLength) {
+  return;
+ }
+ if (passLength < 8) {
+  window.alert("Please enter a larger number.");
+ } 
+ if (passLength > 128) {
+  window.alert("Please enter a smaller number.");
+ }
+
+ var upCase = window.confirm("Would you like to include UPPERCASE LETTERS?");
+
+
+ var lowCase = window.confirm("Would you like to include LOWERCASE letters?");
+
+ var specCase = window.confirm("Would you like to include SPECIAL CHARACTERS?");
+
+}
