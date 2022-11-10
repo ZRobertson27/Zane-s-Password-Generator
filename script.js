cf2627d2f4b753bae1b2e8c4882decaefa9daf19
ZRobertson27 <115191138+ZRobertson27@users.noreply.github.com>
2022-11-10 17:08:46 -0600

    Generator is not functional but buggy.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // variable arrays to pull characters from.
-var numbers = [0,1,2,3,4,5,6,7,8,9];
-var littleLetter = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
-var bigLetter = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
-var specials = ["!","@","#","$","%","^","&","*","-","_","=","+","?"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var littleLetter = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var bigLetter = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+var specials = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+", "?"];
 
 
 // Assignment Code
@@ -25,7 +25,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() { 
-
+  // variable set to = how long you want your password.
   var passLength = window.prompt("How long would you like your password? Min: 8 characters Max: 128 characters.");
  
   if (!passLength) {
@@ -33,9 +33,11 @@ function generatePassword() {
  }
  if (passLength < 8) {
   window.alert("Please enter a larger number.");
+  return;
  } 
  if (passLength > 128) {
   window.alert("Please enter a smaller number.");
+  return;
  }
 
  var upCase = window.confirm("Would you like to include UPPERCASE LETTERS?");
@@ -54,11 +56,9 @@ function generatePassword() {
  
  console.log(passLength);
  console.log(availableCharacters);
- 
+ var characters = " ";
  for (var i = 0; i < passLength; i++) {
-  var characters = availableCharacters [Math.floor(Math.random() * passLength)]
-  console.log(characters);
-  return characters;
-
+  characters += availableCharacters.charAt(Math.floor(Math.random() * passLength));
  }
+ return characters;
 }
