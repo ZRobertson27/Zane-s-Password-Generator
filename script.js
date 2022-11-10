b6833afbeb39c543b2d4a4f5f41f1c3ac7850bf2
ZRobertson27 <115191138+ZRobertson27@users.noreply.github.com>
2022-11-10 14:57:38 -0600

    Largely finished product with bug, WIP.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// variable arrays to pull characters from.
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var littleLetter = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+var bigLetter = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+var specials = ["!","@","#","$","%","^","&","*","-","_","=","+","?"];
+
+
 // Assignment Code
+// Assigns button to variable.
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// Function that runs generatepassword function and displays on screen
 function writePassword() {
   console.log("hello")
   var password = generatePassword();
@@ -15,7 +24,8 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
+function generatePassword() { 
+
   var passLength = window.prompt("How long would you like your password? Min: 8 characters Max: 128 characters.");
  
   if (!passLength) {
@@ -30,9 +40,25 @@ function generatePassword() {
 
  var upCase = window.confirm("Would you like to include UPPERCASE LETTERS?");
 
-
  var lowCase = window.confirm("Would you like to include LOWERCASE letters?");
 
  var specCase = window.confirm("Would you like to include SPECIAL CHARACTERS?");
 
+ var numberCase = window.confirm("Would you like to include NUMBERS?");
+
+ var availableCharacters = "";
+ if (upCase) availableCharacters = availableCharacters.concat(bigLetter);
+ if (lowCase) availableCharacters = availableCharacters.concat(littleLetter);
+ if (specCase) availableCharacters = availableCharacters.concat(specials);
+ if (numberCase) availableCharacters = availableCharacters.concat(numbers);
+ 
+ console.log(passLength);
+ console.log(availableCharacters);
+ 
+ for (var i = 0; i < passLength; i++) {
+  var characters = availableCharacters [Math.floor(Math.random() * passLength)]
+  console.log(characters);
+  return characters;
+
+ }
 }
