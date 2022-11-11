0a71a7b222321ed46142d8449e89af4cd8d054ff
ZRobertson27 <115191138+ZRobertson27@users.noreply.github.com>
2022-11-10 22:56:12 -0600

    Finished working product for now.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// variable arrays to pull characters from.
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var littleLetter = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
-var bigLetter = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
-var specials = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+", "?"];
-
+// variable arrays to pull characters from. Primes default lists, .join used to remove commas.
+var numbersPrime = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = numbersPrime.join('');
+var littleLetterPrime = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var littleLetter = littleLetterPrime.join('');
+var bigLetterPrime = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+var bigLetter = bigLetterPrime.join('');
+var specialsPrime = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+", "?"];
+var specials = specialsPrime.join('');
 
 // Assignment Code
 // Assigns button to variable.
@@ -12,7 +15,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 // Function that runs generatepassword function and displays on screen
 function writePassword() {
-  console.log("hello")
+  //console.log("hello") - for testing
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -49,6 +52,7 @@ function generatePassword() {
  var numberCase = window.confirm("Would you like to include NUMBERS?");
 // If chosen character = true, will add array to variable availableCharacters.
  var availableCharacters = "";
+// For every true below, adds to character pool.
  if (upCase) availableCharacters = availableCharacters.concat(bigLetter);
  if (lowCase) availableCharacters = availableCharacters.concat(littleLetter);
  if (specCase) availableCharacters = availableCharacters.concat(specials);
@@ -58,8 +62,12 @@ function generatePassword() {
  console.log(availableCharacters);
  // Equation to generate the random password.
  var characters = " ";
- for (var i = 0; i < passLength; i++) {
-  characters += availableCharacters.charAt(Math.floor(Math.random() * passLength));
+ 
+for (var i = 0; i < passLength; i++) {
+var random =  Math.floor(Math.random() * availableCharacters.length);
+console.log(random);
+  characters += availableCharacters.charAt(random);
+  console.log(characters);
  }
  console.log(characters);
  return characters;
